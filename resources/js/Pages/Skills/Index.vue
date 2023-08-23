f549e23831d1a0d738003e14c5c328a14b36951f
mustafa toprak <mmustafa.ttoprak@gmail.com>
2023-08-24 02:26:17 +0300

    skill verileri listelendi

--- resources/js/Pages/Skills/Index.vue
+++ resources/js/Pages/Skills/Index.vue
@@ -9,9 +9,47 @@
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-end m-2 p-2">
-                    <Link href="/skills/create"
-                        class="bg-indigo-500 px-4 py-2 text-white hover:bg-indigo-700 rounded-full">New Skill
+                    <Link href="/skills/create" class="bg-indigo-500 px-4 py-2 text-white hover:bg-indigo-700 rounded-full">
+                    New Skill
                     </Link>
+                </div>
+                <div class="relative overflow-x-auto">
+                    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <tr>
+                                <th scope="col" class="px-6 py-3">
+                                    Skill name
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Image
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Edit
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Delete
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="skill in skills.data" :key="skill.id"
+                                class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                <th scope="row"
+                                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    {{ skill.name }}
+                                </th>
+                                <td class="px-6 py-4" >
+                                    <img :src="skill.image" alt="" class="w-12 h-12 rounded-full">
+                                </td>
+                                <td class="px-6 py-4">
+                                    Laptop
+                                </td>
+                                <td class="px-6 py-4">
+                                    $2999
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
@@ -22,4 +60,8 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head, Link } from '@inertiajs/vue3';
+
+defineProps({
+    skills: Object,
+})
 </script>
